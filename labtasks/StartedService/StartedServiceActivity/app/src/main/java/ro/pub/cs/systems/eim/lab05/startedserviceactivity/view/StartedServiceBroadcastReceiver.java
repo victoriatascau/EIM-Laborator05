26ff91941c0e7f2d3ddeaa07c74ee6447e759625
victoriatascau <vickytascau@gmail.com>
2021-04-09 13:24:38 +0300

    Implement tasks 1-9

--- labtasks/StartedService/StartedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceBroadcastReceiver.java
+++ labtasks/StartedService/StartedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceBroadcastReceiver.java
@@ -5,11 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.TextView;
 
+import ro.pub.cs.systems.eim.lab05.startedserviceactivity.general.Constants;
+
 public class StartedServiceBroadcastReceiver extends BroadcastReceiver {
 
     private TextView messageTextView;
 
     // TODO: exercise 9 - default constructor
+    public StartedServiceBroadcastReceiver() {
+        this.messageTextView = null;
+    }
 
     public StartedServiceBroadcastReceiver(TextView messageTextView) {
         this.messageTextView = messageTextView;
@@ -19,8 +24,28 @@ public class StartedServiceBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: exercise 7 - get the action and the extra information from the intent
         // and set the text on the messageTextView
+        String action = intent.getAction();
+        String data = null;
+
+        if (Constants.ACTION_STRING.equals(action)) {
+            data = intent.getStringExtra(Constants.DATA);
+        }
+        if  (Constants.ACTION_INTEGER.equals(action)) {
+            data = intent.getStringExtra(Constants.DATA);
+        }
+        if (Constants.ACTION_ARRAY_LIST.equals(Constants.DATA)) {
+            data = intent.getStringExtra(Constants.DATA);
+        }
+
+        if (messageTextView != null) {
+            messageTextView.setText(messageTextView.getText().toString() + '\n' + data);
+        }
 
         // TODO: exercise 9 - restart the activity through an intent
         // if the messageTextView is not available
+        Intent startedServiceActivityIntent = new Intent(context, StartedServiceActivity.class);
+        startedServiceActivityIntent.putExtra(Constants.MESSAGE, data);
+        startedServiceActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        context.startActivity(startedServiceActivityIntent);
     }
 }
